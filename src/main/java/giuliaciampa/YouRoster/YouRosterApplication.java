465e74a8f9775528d9acd42f5e695b35f27e8872
giulia-ciampa <giulia.ciampa7@gmail.com>
2026-07-31 15:43:44 +0200

    Initial commit

---
+++ src/main/java/giuliaciampa/YouRoster/YouRosterApplication.java
@@ -0,0 +1,13 @@
+package giuliaciampa.YouRoster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouRosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouRosterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/giuliaciampa/YouRoster/YouRosterApplicationTests.java
@@ -0,0 +1,13 @@
+package giuliaciampa.YouRoster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouRosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
