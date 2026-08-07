9445a2a16a87da006291af1263b4d05c6460b87d
giulia-ciampa <giulia.ciampa7@gmail.com>
2026-08-07 16:26:03 +0200

    Update RoleController.java

--- src/main/java/giuliaciampa/YouRoster/controllers/RoleController.java
+++ src/main/java/giuliaciampa/YouRoster/controllers/RoleController.java
@@ -1,11 +1,28 @@
 package giuliaciampa.YouRoster.controllers;
 
+import giuliaciampa.YouRoster.entities.Role;
+import giuliaciampa.YouRoster.services.RoleService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/roles")
 public class RoleController {
 
+    private final RoleService roleService;
 
+
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'HR')")
+    public List<Role> getAllRoles() {
+        return roleService.findAll();
+    }
 }
